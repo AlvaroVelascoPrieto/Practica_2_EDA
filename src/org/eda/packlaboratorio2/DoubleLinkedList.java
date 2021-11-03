a1382e20b4eed77959751f452cd9ec1cd4dd309f
alvar <alvaritovelasco2002@gmail.com>
2021-11-03 12:58:58 +0100

    Un ultimo cambio al iterador

--- src/org/eda/packlaboratorio2/DoubleLinkedList.java
+++ src/org/eda/packlaboratorio2/DoubleLinkedList.java
@@ -1,6 +1,5 @@
 package org.eda.packlaboratorio2;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 public class DoubleLinkedList<T> implements ListADT<T> {
 
@@ -15,18 +14,18 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 		descr = "";
 		count = 0;
 	}
-	
+
 	public void setDescr(String nom) {
-	  descr = nom;
+		descr = nom;
 	}
 
 	public String getDescr() {
-	  return descr;
+		return descr;
 	}
 
 	public T removeFirst() {
-	// Elimina el primer elemento de la lista
-        // Precondici�n: 
+		// Elimina el primer elemento de la lista
+		// Precondici�n:
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
 		T devolver;
 		if (last==null){
@@ -46,9 +45,9 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	}
 
 	public T removeLast() {
-	// Elimina el �ltimo elemento de la lista
-        // Precondici�n: 
-			// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+		// Elimina el �ltimo elemento de la lista
+		// Precondici�n:
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
 		T devolver;
 		if (last==null){
 			return null;
@@ -69,7 +68,7 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 
 
 	public T remove(T elem) {
-	//Elimina un elemento concreto de la lista
+		//Elimina un elemento concreto de la lista
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
 		//Coste n cuidado
 		Node<T> current;
@@ -97,22 +96,22 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	}
 
 	public T first() {
-	//Da acceso al primer elemento de la lista
-	      if (isEmpty()){
-	          return null;}
-	      else {return last.next.data;}
+		//Da acceso al primer elemento de la lista
+		if (isEmpty()){
+			return null;}
+		else {return last.next.data;}
 	}
 
 	public T last() {
-	//Da acceso al �ltimo elemento de la lista
-	      if (isEmpty()) {
-			  return null;
-		  }
-	      else {return last.data;}
+		//Da acceso al �ltimo elemento de la lista
+		if (isEmpty()) {
+			return null;
+		}
+		else {return last.data;}
 	}
 
 	public boolean contains(T elem) {
-	//Determina si la lista contiene un elemento concreto
+		//Determina si la lista contiene un elemento concreto
 		if (isEmpty())
 			return false;
 		else {
@@ -131,7 +130,7 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	}
 
 	public T find(T elem) {
-	//Determina si la lista contiene un elemento concreto, y develve su referencia, null en caso de que no est�
+		//Determina si la lista contiene un elemento concreto, y develve su referencia, null en caso de que no est�
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
 		if (this.isEmpty())
 			return (null);
@@ -149,55 +148,59 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 		return null;
 	}
 
-	public boolean isEmpty() 
+	public boolean isEmpty()
 	//Determina si la lista est� vac�a
 	{ return last == null;};
-	
-	public int size() 
+
+	public int size()
 	//Determina el n�mero de elementos de la lista
 	{ return count;};
-	
-	/** Return an iterator to the stack that iterates through the items . */ 
-	public Iterator<T> iterator() { return new ListIterator(); } 
 
-	   // an iterator, doesn't implement remove() since it's optional 
-	   private class ListIterator implements Iterator<T> { 
+	/** Return an iterator to the stack that iterates through the items . */
+	public Iterator<T> iterator() { return new ListIterator(); }
+
+	// an iterator, doesn't implement remove() since it's optional
+	private class ListIterator implements Iterator<T> {
 
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-			private Node<T> aux=last.next;
-
-
-		   @Override
-		   public boolean hasNext() {
-			   if (aux!=last){
-				   return true;
-			   }
-			   else {return false;}
-		   }
-
-		   public T next(){
-				T a = aux.data;
-				aux = aux.next;
-				return a;
-			}
-
-
-	   } // private class
-		
-		
-         public void visualizarNodos() {
-			System.out.println(this.toString());
-		}
+		private Node<T> aux=last.next;
+		private boolean ult=false;
 
 		@Override
-		public String toString() {
-			String result = new String();
-			Iterator<T> it = iterator();
-			while (it.hasNext()) {
-				T elem = it.next();
-				result = result + "[" + elem.toString() + "] \n";
-			}	
-			return "DoubleLinkedList " + result + "]";
+		public boolean hasNext() {
+			if(aux!=last && !ult) {
+				return true;
+			}
+			else if (!ult) {
+				ult=true;
+				return true;
+			}
+			else {return false;}
 		}
+
+		public T next(){
+			T a = aux.data;
+			aux = aux.next;
+			return a;
+		}
+
+
+	} // private class
+
+
+	public void visualizarNodos() {
+		System.out.println(this.toString());
+	}
+
+	@Override
+	public String toString() {
+		String result = new String();
+		Iterator<T> it = iterator();
+		while (it.hasNext()) {
+			T elem = it.next();
+			result = result + "[" + elem.toString() + "] \n";
+		}
+		return "DoubleLinkedList " + result + "]";
+	}
 
 }
